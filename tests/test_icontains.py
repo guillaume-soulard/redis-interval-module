f9d28dca921f6c0c8013ccf39c0188c8ffa9ab18
gsoulard <guillaume.soulard@effiliation.com>
2021-05-28 18:41:16 +0200

    Add some tests on icontains and ioverlaps commands

--- tests/test_icontains.py
+++ tests/test_icontains.py
@@ -18,7 +18,35 @@ class TestIContains():
         self.env.cmd('FLUSHALL')
         self.env.expect('icontains', 'intervals', 'test').error()
 
+    def test_icontains_should_return_empty_when_key_is_empty(self):
+        self.env.cmd('FLUSHALL')
+        self.env.expect('icontains', 'intervals', '0').equal([])
+
     def test_icontains_should_return_one_member(self):
         self.env.cmd('FLUSHALL')
         self.env.cmd('iadd', 'intervals', '0,1', 'i1')
         self.env.expect('icontains', 'intervals', '0').equal([[b'i1', b'1', b'0', b'1', b'1']])
+
+    def test_icontains_should_return_empty_when_value_is_out_of_interval(self):
+        self.env.cmd('FLUSHALL')
+        self.env.cmd('iadd', 'intervals', '0,1', 'i1')
+        self.env.expect('icontains', 'intervals', '2').equal([])
+
+    def test_icontains_should_return_empty_when_value_is_equals_to_upper_and_upper_is_excluded(self):
+        self.env.cmd('FLUSHALL')
+        self.env.cmd('iadd', 'intervals', '0,1[', 'i1')
+        self.env.expect('icontains', 'intervals', '1').equal([])
+
+    def test_icontains_should_return_empty_when_value_is_equals_to_lower_and_lower_is_excluded(self):
+        self.env.cmd('FLUSHALL')
+        self.env.cmd('iadd', 'intervals', ']0,1', 'i1')
+        self.env.expect('icontains', 'intervals', '0').equal([])
+
+    def test_icontains_should_return_2_members(self):
+        self.env.cmd('FLUSHALL')
+        self.env.cmd('iadd', 'intervals', '0,3', 'i1')
+        self.env.cmd('iadd', 'intervals', '0,5', 'i2')
+        res = self.env.cmd('icontains', 'intervals', '2')
+        self.env.assertEqual(len(res), 2)
+        self.env.assertContains([b'i2', b'1', b'0', b'1', b'5'], res)
+        self.env.assertContains([b'i1', b'1', b'0', b'1', b'3'], res)
---
+++ tests/test_ioverlaps.py
@@ -0,0 +1,63 @@
+from RLTest import Env
+
+
+class TestIOverlaps():
+
+    def __init__(self):
+        self.env = Env()
+
+    def test_ioverlaps_should_return_an_error_with_0_parameters(self):
+        self.env.cmd('FLUSHALL')
+        self.env.expect('ioverlaps').error()
+
+    def test_ioverlaps_should_return_an_error_with_1_parameter(self):
+        self.env.cmd('FLUSHALL')
+        self.env.expect('ioverlaps', 'intervals').error()
+
+    def test_ioverlaps_should_return_the_interval_when_parameter_interval_is_in_the_interval(self):
+        self.env.cmd('FLUSHALL')
+        self.env.cmd('iadd', 'intervals', '5,10', 'i1')
+        self.env.expect('ioverlaps', 'intervals', '6,7').equal(
+            [[b'i1', b'1', b'5', b'1', b'10']])
+
+    def test_ioverlaps_should_return_the_interval_when_interval_is_in_the_parameter_interval(self):
+        self.env.cmd('FLUSHALL')
+        self.env.cmd('iadd', 'intervals', '5,10', 'i1')
+        self.env.expect('ioverlaps', 'intervals', '0,20').equal(
+            [[b'i1', b'1', b'5', b'1', b'10']])
+
+    def test_ioverlaps_should_return_the_interval_when_parameter_interval_overlaps_lower_bound(self):
+        self.env.cmd('FLUSHALL')
+        self.env.cmd('iadd', 'intervals', '5,10', 'i1')
+        self.env.expect('ioverlaps', 'intervals', '0,7').equal(
+            [[b'i1', b'1', b'5', b'1', b'10']])
+
+    def test_ioverlaps_should_return_the_interval_when_parameter_interval_overlaps_upper_bound(self):
+        self.env.cmd('FLUSHALL')
+        self.env.cmd('iadd', 'intervals', '5,10', 'i1')
+        self.env.expect('ioverlaps', 'intervals', '7,15').equal(
+            [[b'i1', b'1', b'5', b'1', b'10']])
+
+    def test_ioverlaps_should_return_the_interval_when_parameter_interval_abuts_lower_bound(self):
+        self.env.cmd('FLUSHALL')
+        self.env.cmd('iadd', 'intervals', '5,10', 'i1')
+        self.env.expect('ioverlaps', 'intervals', '0,5').equal(
+            [[b'i1', b'1', b'5', b'1', b'10']])
+
+    def test_ioverlaps_should_return_the_interval_when_parameter_interval_abuts_upper_bound(self):
+        self.env.cmd('FLUSHALL')
+        self.env.cmd('iadd', 'intervals', '5,10', 'i1')
+        self.env.expect('ioverlaps', 'intervals', '10,15').equal(
+            [[b'i1', b'1', b'5', b'1', b'10']])
+
+    def test_ioverlaps_should_return_the_interval_when_parameter_interval_abuts_lower_bound_and_lower_not_included(self):
+        self.env.cmd('FLUSHALL')
+        self.env.cmd('iadd', 'intervals', ']5,10', 'i1')
+        self.env.expect('ioverlaps', 'intervals', '0,5').equal(
+            [])
+
+    def test_ioverlaps_should_return_the_interval_when_parameter_interval_abuts_upper_bound_and_upper_not_included(self):
+        self.env.cmd('FLUSHALL')
+        self.env.cmd('iadd', 'intervals', '5,10[', 'i1')
+        self.env.expect('ioverlaps', 'intervals', '10,15').equal(
+            [])
